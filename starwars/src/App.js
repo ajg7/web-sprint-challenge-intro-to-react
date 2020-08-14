73532a23ceb720cb4c5582da1e55a97965d67398
ajg7 <ajgebara@gmail.com>
2020-08-14 10:44:58 -0500

    fetched the data successfully and set character object state

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +9,29 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [characterObj, setCharacterObj] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/")
+    .then(response => {
+      const {name, homeWorld, birthYear, gender, films} = response.data;
+      const characterObj = {
+        name: name,
+        homeWorld: homeWorld,
+        birthYear: birthYear,
+        gender: gender,
+        films: films,
+      }
+      console.log(gender);
+      setCharacterObj(characterObj);
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }, [])
+
 
   return (
     <div className="App">
